279b8c4650352ac1ba740f9e2cdf2f6892fff65c
ShamNeko <mkdvlp01@gmail.com>
2025-04-21 23:15:10 +0900

    for of 構文はじまり

---
+++ app.js
@@ -0,0 +1,13 @@
+/* =================================================
+// for of構文
+const seatingChart = [
+    ["mori", "saito", "suzuki", "kanazawa"],
+    ["xxx", "zzz"]
+];
+
+for (let row of seatingChart) {
+    for (let student of row) {
+        console.log(student);
+    }
+}
+================================================= */
